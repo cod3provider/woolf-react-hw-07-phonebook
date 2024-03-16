9569f3dbd1343b103430cf4a678b94b056ce7349
Ilya Bondarchuk <cod3provider@gmail.com>
2024-03-16 20:58:20 +0100

    refactored contactsSlice with addMatcher

--- src/store/contacts/contactsSlice.js
+++ src/store/contacts/contactsSlice.js
@@ -11,32 +11,19 @@ const contactsSlice = createSlice({
   },
   extraReducers: builder => {
     builder
-    // .addCase(fetchContacts.pending, handlePendingContacts)
     .addCase(fetchContacts.fulfilled, (state, { payload }) => {
-      // state.isLoading = false;
-      // state.error = null;
       state.items = payload;
     })
-    // .addCase(fetchContacts.rejected, handleRejectedContacts)
-    // .addCase(addContact.pending, handlePendingContacts)
     .addCase(addContact.fulfilled, (state, { payload }) => {
-      // state.isLoading = false;
-      // state.error = null;
       state.items.push(payload);
     })
-    // .addCase(addContact.rejected, handleRejectedContacts)
-    // .addCase(deleteContact.pending, handlePendingContacts)
     .addCase(deleteContact.fulfilled, (state, { payload }) => {
-      // state.isLoading = false;
-      // state.error = null;
       const index = state.items.findIndex(item => item.id === payload);
       state.items.splice(index, 1);
     })
-    // .addCase(deleteContact.rejected, handleRejectedContacts)
     .addMatcher(action => action.type.endsWith('fulfilled'), handleFulfilledContacts)
     .addMatcher(action => action.type.endsWith('pending'), handlePendingContacts)
     .addMatcher(action => action.type.endsWith('rejected'), handleRejectedContacts);
-    ;
   },
 });
 
