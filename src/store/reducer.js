e71dc303320a7bdfd0b04688785390abfd6a8e8e
Ilya Bondarchuk <cod3provider@gmail.com>
2024-03-17 10:29:44 +0100

    refactored store, removed redux-persist

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,17 +1,7 @@
 import { filterReducer } from './filterSlice';
 import { contactsReducer } from './contacts/contactsSlice';
-import { persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-
-const persistConfig = {
-  key: 'root',
-  storage,
-  blacklist: 'filter',
-};
-
-const persistedReducer = persistReducer(persistConfig, contactsReducer);
 
 export const reducer = {
-  contacts: persistedReducer,
+  contacts: contactsReducer,
   filter: filterReducer,
 };
